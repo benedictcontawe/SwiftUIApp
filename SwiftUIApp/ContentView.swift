91d8f7de93e716e4e5dc4249b45b9ef392065cde
Benedict Contawe <benedictcontawe@gmail.com>
2024-07-22 23:03:27 +0800

    Move @State variable to view model

--- SwiftUIApp/ContentView.swift
+++ SwiftUIApp/ContentView.swift
@@ -10,10 +10,6 @@ import SwiftData
 
 struct ContentView: View {
     @ObservedObject var viewModel : ViewModel = ViewModel()
-    @State private var toggleState : Bool = true
-    @State private var pickerState : Int = 0
-    @State private var stepperState : Int = 5
-    @State private var sliderState : Float = 5.0
 
     var body: some View {
         VStack {
@@ -29,28 +25,28 @@ struct ContentView: View {
             ) {
                 Text("Send Data")
             }
-            Toggle("Toggle", isOn: $toggleState).onChange(of: toggleState) { oldValue, newValue in
+            Toggle("Toggle", isOn: viewModel.$toggleState).onChange(of: viewModel.toggleState) { oldValue, newValue in
                 viewModel.setSwitchChecked(isChecked: newValue)
             }
             Picker(
-                selection: $pickerState,
+                selection: viewModel.$pickerState,
                 label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/
             ) {
                 /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
                 Text("2").tag(2)
-            }.onChange(of: pickerState) { oldValue, newValue in
+            }.onChange(of: viewModel.pickerState) { oldValue, newValue in
                 viewModel.setData(data: "Picker set to \(newValue)")
             }.pickerStyle(.automatic)
             Stepper(
-                value: $stepperState,
+                value: viewModel.$stepperState,
                 in: 0...10
             ) {
-                Text("Stepper \(stepperState)")
-            }.onChange(of: stepperState) { oldValue, newValue in
+                Text("Stepper \(viewModel.stepperState)")
+            }.onChange(of: viewModel.stepperState) { oldValue, newValue in
                 viewModel.setData(data: "Stepper set to \(newValue)")
             }
             Slider(
-                value: $sliderState,
+                value: viewModel.$sliderState,
                 in: 0...10,
                 step: 1,
                 onEditingChanged: { data in
@@ -61,7 +57,7 @@ struct ContentView: View {
                 label: {
                     Text("Slider")
                 }
-            ).onChange(of: sliderState) { oldValue, newValue in
+            ).onChange(of: viewModel.sliderState) { oldValue, newValue in
                 viewModel.setData(data: "Slider set to \(newValue)")
             }
         }
