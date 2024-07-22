7f29560e7d8890030c9a065c6d8439d87bbbce13
Benedict Contawe <benedictcontawe@gmail.com>
2024-07-22 22:54:42 +0800

    added view model and data model

--- SwiftUIApp/ContentView.swift
+++ SwiftUIApp/ContentView.swift
@@ -9,38 +9,45 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @State private var progressState : Double = 0.5
+    @ObservedObject var viewModel : ViewModel = ViewModel()
     @State private var toggleState : Bool = true
+    @State private var pickerState : Int = 0
     @State private var stepperState : Int = 5
     @State private var sliderState : Float = 5.0
 
     var body: some View {
         VStack {
             HStack {
-                Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
-                ProgressView(value: progressState)
+                Text(viewModel.getData())
+                ProgressView(value: viewModel.getProgressData())
             }.fixedSize(horizontal: false, vertical: false)
             Divider()
             Button (
                 action: {
-                    
+                    viewModel.setData(data: "Button was Clicked")
                 }
             ) {
                 Text("Send Data")
             }
-            Toggle("Toggle", isOn: $toggleState)
+            Toggle("Toggle", isOn: $toggleState).onChange(of: toggleState) { oldValue, newValue in
+                viewModel.setSwitchChecked(isChecked: newValue)
+            }
             Picker(
-                selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/,
+                selection: $pickerState,
                 label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/
             ) {
                 /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
                 Text("2").tag(2)
+            }.onChange(of: pickerState) { oldValue, newValue in
+                viewModel.setData(data: "Picker set to \(newValue)")
             }.pickerStyle(.automatic)
             Stepper(
                 value: $stepperState,
                 in: 0...10
             ) {
-                Text("Stepper")
+                Text("Stepper \(stepperState)")
+            }.onChange(of: stepperState) { oldValue, newValue in
+                viewModel.setData(data: "Stepper set to \(newValue)")
             }
             Slider(
                 value: $sliderState,
@@ -54,7 +61,9 @@ struct ContentView: View {
                 label: {
                     Text("Slider")
                 }
-            )
+            ).onChange(of: sliderState) { oldValue, newValue in
+                viewModel.setData(data: "Slider set to \(newValue)")
+            }
         }
         .frame(maxHeight: .infinity)
         
---
+++ SwiftUIApp/ViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  ViewModel.swift
+//  SwiftUIApp
+//
+//  Created by Benedict Contawe on 7/22/24.
+//
+
+import SwiftUI
+
+class ViewModel : ObservableObject {
+    @Published public var model : [CustomModel]
+    @Published private var data : String
+    @Published private var progressData : Float
+    
+    init() {
+        model = []
+        data = "Data"
+        progressData = 0.0
+    }
+    
+    func setData(data : String) {
+        self.data = data
+    }
+    
+    func getData() -> String {
+        return self.data
+    }
+    
+    func setProgressData(progressData : Float) {
+        self.progressData = progressData
+    }
+    
+    func getProgressData() -> Float {
+        return self.progressData
+    }
+    
+    func setSwitchChecked(isChecked : Bool) {
+        if isChecked {
+            setData(data: "Toggle is On")
+        } else {
+            setData(data: "Toggle is Off")
+        }
+    }
+    
+    func shuffleModel() {
+        model.shuffle()
+    }
+    
+    func reverseOrder() {
+        model.reverse()
+    }
+    
+    deinit {
+        
+    }
+}
