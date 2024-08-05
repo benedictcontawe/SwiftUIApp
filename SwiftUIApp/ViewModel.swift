2c97ba0b8f98f61c26d1a14bd74e64466239fbe6
Benedict Contawe <benedictcontawe@gmail.com>
2024-08-05 22:17:43 +0800

    added Combine feature

--- SwiftUIApp/ViewModel.swift
+++ SwiftUIApp/ViewModel.swift
@@ -6,18 +6,37 @@
 //
 
 import SwiftUI
+import Combine
 
-internal final class ViewModel : ObservableObject {
-    @Published public var models : [TabModel]
+internal final class ViewModel: ObservableObject {
+    @Published public var models : [TabModel] = []
+    private var cancellables : Set<AnyCancellable> = Set<AnyCancellable>()
+    private let modelSubject : PassthroughSubject<TabModel, Never> = PassthroughSubject<TabModel, Never>()
+
     init() {
-        models = [
+        let initialModels : [TabModel] = [
             TabModel(text: "AAA", image: "phone.fill"),
             TabModel(text: "BBB", image: "person.2.fill"),
             TabModel(text: "CCC", image: "slider.horizontal.3")
         ]
+        modelSubject
+            .delay(for: .seconds(2), scheduler: DispatchQueue.main)
+            .sink { [weak self] model in
+                self?.models.append(model)
+            }
+            .store(in: &cancellables)
+        addModelsWithDelay(initialModels)
+    }
+    
+    private func addModelsWithDelay(_ newModels: [TabModel]) {
+        for (index, model) in newModels.enumerated() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) * 2.0) {
+                self.modelSubject.send(model)
+            }
+        }
     }
     
     deinit {
-        
+        cancellables.forEach { $0.cancel() }
     }
 }
